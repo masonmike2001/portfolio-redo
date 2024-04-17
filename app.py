a775d4b9c3dadc0c48aadb751af7538c265f0538
Michael Mason <masonmike2001@gmail.com>
2024-04-16 21:43:23 -0400

    completes front page, adds project item to portfolio

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route('/work')
+@app.route('/portfolio')
 def work():
-    return render_template("work.html")
+    return render_template("portfolio.html")
 
 
 @app.route('/bio')
